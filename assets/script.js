53ea96878c89b36090c884602d63851a7a79ee6a
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2022-10-19 15:02:24 -0600

    Added basic CSS

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//Pseudocode
+
+//2. After entering a city into the searchbar, that city's name is added to a "search history"
+    // Also the current weather conditions for that city will come up in a main section: This will include the city's: Name, date, icon of weather conditions, temperature, humidity and wind speed.
+    //Another section will include a five day (future) forcast displaying all of the same information as the current temperature section.
+//3. When clicking a city in the "search history" it will take me back to the page with all of the information above.
+
+
